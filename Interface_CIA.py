efc122807857043e203e7097961772a3c0e2aa5a
Boris FAGBEDJI <camillefagbedji69@gmail.com>
2026-03-20 10:47:05 +0100

    Update Interface_CIA.py

--- Interface_CIA.py
+++ Interface_CIA.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+import os
+st.write("Fichiers dispo dans l’app :", os.listdir())
+
 # ===== CONFIG =====
 st.set_page_config(
     page_title="Notes CIA-FA",
